2e11368a8fd349722be0c5c427f4ecbef33078e3
Roma <oksana.oksana.oksana9@yandex.ru>
2023-11-15 00:28:22 +0300

    Переделываю

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -2,10 +2,24 @@ import java.io.File;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        try{                                        // пытаться выполнить какой-то код
+            var z = calculate();
+            System.out.println(z);
+            System.out.println("Hello, world!");
+        } catch (ArithmeticException exception){ // исключение имеет тип арифметики exception (мы знаем об этом из лога падения проги)
+            exception.printStackTrace();           // код который выполнится , когда возникнет исключение выведем инфу о стеке вызовов
+        }
+    }
 
-        var configFile = new File("sandbox/build.gradle");
-        System.out.println(configFile.getAbsolutePath());
-        System.out.println(configFile.exists());
+    private static int calculate() {
+        var x=1;
+        var y=1;
+        var z = devide(x, y);
+        return z;
+    }
+
+    private static int devide(int x, int y) {
+        var z= x / y;
+        return z;
     }
 }
